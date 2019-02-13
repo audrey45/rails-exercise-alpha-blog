3caeb3ed90f5546f0c11a4b6718d0fc02eb1e2a6
audrey45 <arceniokas@gmail.com>
2019-02-13 20:57:15 +0800

    add validations to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_120357) do
+ActiveRecord::Schema.define(version: 2019_02_13_122043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2019_02_13_120357) do
     t.string "title"
     t.text "description"
     t.datetime "created_at"
-    t.datetime "update_at"
+    t.datetime "updated_at"
   end
 
 end
